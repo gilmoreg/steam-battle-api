c43eb80832d77902db74d302466b2a7baa47efbb
gilmoreg@live.com <gilmoreg@live.com>
2017-03-01 18:57:16 -0600

    Axios/Vanity route

--- config.js
+++
@@ -1,2 +0,0 @@
-exports.STEAM_API_KEY = process.env.STEAM_API_KEY ||
-    global.STEAM_API_KEY;
--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require('express')
 const cors = require('cors');
+const axios = require('axios');
 let app = express();
 app.use(cors());
+const STEAM_API_KEY = process.env.STEAM_API_KEY || global.STEAM_API_KEY;
+
+app.get('/vanity/:url', (req,res) => {
+    const vanity = req.params.url;
+    const url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${vanity}`;
+    axios.get(url)
+        .then(res=>{
+            res.status(200).json( { "response": res }  );
+        })
+        .catch(err=>{
+            res.json( { 'Error': err } );
+        });
+});
+
+app.listen(8080);
+
+/*
+vanity
+playerSummaries
+ownedGames
+achievements
+achievementPercents
+
+*/
