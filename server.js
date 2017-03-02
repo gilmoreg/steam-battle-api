7856cd8151716cfd912a3542744f368abb1d119d
gilmoreg@live.com <gilmoreg@live.com>
2017-03-01 19:17:33 -0600

    Vanity route

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const axios = require('axios');
 let app = express();
 app.use(cors());
-const STEAM_API_KEY = process.env.STEAM_API_KEY || global.STEAM_API_KEY;
+const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 app.get('/vanity/:url', (req,res) => {
     const vanity = req.params.url;
-    const url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${vanity}`;
+    const url = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAM_API_KEY}&vanityurl=${vanity}`;
     axios.get(url)
-        .then(res=>{
-            res.status(200).json( { "response": res }  );
+        .then(response=>{
+            console.log(response);
+            res.status(200).json( {"steamid": response.data.response.steamid} );
         })
         .catch(err=>{
-            res.json( { 'Error': err } );
+            console.log(err);
+            res.json( {'/vanity/:url error': err } );
         });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 9000);
 
 /*
 vanity
