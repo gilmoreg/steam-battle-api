3b28a4dbeaed4bfd7c659999154f6e8d98056965
gilmoreg@live.com <gilmoreg@live.com>
2017-03-07 18:28:58 -0600

    Moxios test

--- test/test-server.js
+++ test/test-server.js
@@ -6,60 +6,18 @@ const chaiHttp = require('chai-http');
 const moxios = require('moxios');
 
 const { app, runServer, closeServer } = require('../server');
+const fakes = require('./fakes');
 require('dotenv').config();
 
 const should = chai.should();
 chai.use(chaiHttp);
 
-const fakeOwnedGames = {
-  response: {
-    game_count: 2,
-    games: [
-      {
-        appid: 105600,
-        playtime_2weeks: 1,
-        playtime_forever: 4,
-      },
-      {
-        appid: 282900,
-        playtime_forever: 3472,
-      },
-    ] }
-};
-
-const fakeVanityGoodResponse = {
-  response: {
-    steamid: '76561198007908897',
-    success: 1
-  }
-};
-
-const fakeVanityBadResponse = {
-  response: {
-    success: 42,
-    message: 'No match',
-  }
-};
-
-const fakeProfileResponse = {
-  response: {
-    players: [
-      {
-        steamid: '0000',
-        personaname: 'test',
-        profileurl: 'test',
-        avatarfull: 'test',
-      }
-    ]
-  }
-};
-
 describe('GET /checkid/:id', () => {
-  before(() => {
+  beforeEach(() => {
     runServer();
     moxios.install();
   });
-  after(() => {
+  afterEach(() => {
     moxios.uninstall();
     closeServer();
   });
@@ -67,40 +25,62 @@ describe('GET /checkid/:id', () => {
   it('should should validate a known good id', (done) => {
     moxios.stubRequest(/.*(GetOwnedGames).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeOwnedGames),
+      responseText: JSON.stringify(fakes.ownedGames),
     });
-    return chai.request(app)
+    chai.request(app)
       .get('/checkid/76561198007908897')
       .then((res) => {
         res.should.have.status(200);
         res.body.steamid.should.equal('76561198007908897');
+        done();
       })
       .catch((err) => {
-        console.log('err', err);
-        true.should.equal(false);
+        should.fail(err);
         done();
       });
   });
-/* 
-  it('should should validate a known good vanity url', (done) =>
+
+  it('should should validate a known good vanity url', (done) => {
+    moxios.stubRequest(/.*(GetOwnedGames).*/, {
+      status: 200,
+      responseText: JSON.stringify(fakes.ownedGames),
+    });
+    moxios.stubRequest(/.*(Vanity).*/, {
+      status: 200,
+      responseText: JSON.stringify(fakes.vanityGoodResponse),
+    });
     chai.request(app)
       .get('/checkid/solitethos')
       .then((res) => {
         res.should.have.status(200);
         res.body.steamid.should.equal('76561198007908897');
+        done();
       })
-  );
+      .catch((err) => {
+        should.fail(err);
+        done();
+      });
+  });
 
-  it('should should return empty on a known bad id', (done) =>
+  it('should should return empty on a known bad id', (done) => {
+    moxios.stubRequest(/.*(Vanity).*/, {
+      status: 200,
+      responseText: JSON.stringify(fakes.vanityBadResponse),
+    });
     chai.request(app)
-      .get('/checkid/aaaa')
+      .get('/checkid/00zaz000')
       .then((res) => {
         res.should.have.status(204);
         res.body.should.eql({});
+        done();
       })
-  );
+      .catch((err) => {
+        should.fail(err);
+        done();
+      });
+  });
 });
-
+/*
 describe('GET /player/:id', () => {
   before(() => runServer());
   after(() => closeServer());
@@ -127,6 +107,7 @@ describe('GET /player/:id', () => {
         res.should.have.status(500);
       })
   );
-  */
+ 
 });
 
+ */
--- test/test-steam.js
+++ test/test-steam.js
@@ -4,54 +4,12 @@
 const chai = require('chai');
 const moxios = require('moxios');
 const Steam = require('../steam');
+const fakes = require('./fakes');
 
 require('dotenv').config();
 
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
 const should = chai.should();
-
-const fakeOwnedGames = {
-  response: {
-    game_count: 2,
-    games: [
-      {
-        appid: 105600,
-        playtime_2weeks: 1,
-        playtime_forever: 4,
-      },
-      {
-        appid: 282900,
-        playtime_forever: 3472,
-      },
-    ] }
-};
-
-const fakeVanityGoodResponse = {
-  response: {
-    steamid: '76561198007908897',
-    success: 1
-  }
-};
-
-const fakeVanityBadResponse = {
-  response: {
-    success: 42,
-    message: 'No match',
-  }
-};
-
-const fakeProfileResponse = {
-  response: {
-    players: [
-      {
-        steamid: '0000',
-        personaname: 'test',
-        profileurl: 'test',
-        avatarfull: 'test',
-      }
-    ]
-  }
-};
 
 describe('Steam functions', () => {
   beforeEach(() => moxios.install());
@@ -63,7 +21,7 @@ describe('Steam functions', () => {
     // and one to check if the user owns any games)
     moxios.stubRequest(/.*(GetOwnedGames).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeOwnedGames),
+      responseText: JSON.stringify(fakes.ownedGames),
     });
     moxios.wait(() => {
       Steam.checkid('76561198007908897').then((response) => {
@@ -80,11 +38,11 @@ describe('Steam functions', () => {
   it('checkid should validate a known good vanity url', (done) => {
     moxios.stubRequest(/.*(GetOwnedGames).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeOwnedGames),
+      responseText: JSON.stringify(fakes.ownedGames),
     });
     moxios.stubRequest(/.*(Vanity).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeVanityGoodResponse),
+      responseText: JSON.stringify(fakes.vanityGoodResponse),
     });
     moxios.wait(() => {
       Steam.checkid('solitethos').then((response) => {
@@ -101,11 +59,11 @@ describe('Steam functions', () => {
   it('checkid should return null on a known bad id', (done) => {
     moxios.stubRequest(/.*(GetOwnedGames).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeOwnedGames),
+      responseText: JSON.stringify(fakes.ownedGames),
     });
     moxios.stubRequest(/.*(Vanity).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeVanityBadResponse),
+      responseText: JSON.stringify(fakes.vanityBadResponse),
     });
     moxios.wait(() => {
       Steam.checkid('aaaa').then(() => {
@@ -122,11 +80,11 @@ describe('Steam functions', () => {
   it('player should return valid data for a known good id', (done) => {
     moxios.stubRequest(/.*(GetOwnedGames).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeOwnedGames),
+      responseText: JSON.stringify(fakes.ownedGames),
     });
     moxios.stubRequest(/.*(Summaries).*/, {
       status: 200,
-      responseText: JSON.stringify(fakeProfileResponse),
+      responseText: JSON.stringify(fakes.profileResponse),
     });
     Steam.player('0000')
       .then((player) => {
